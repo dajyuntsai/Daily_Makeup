c9bfd2c496349cac0261ff3d2b20e933a3346d22
Hueijyun <ang7534387@gmail.com>
2020-01-27 17:00:12 +0800

    fitst commit

---
+++ Daily_Makeup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Daily_Makeup
+//
+//  Created by Hueijyun  on 2020/1/25.
+//  Copyright © 2020 Hueijyun . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
